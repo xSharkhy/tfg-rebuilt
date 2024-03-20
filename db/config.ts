752c21e4db40895496b5ecf5409deee75ad1fd5d
Ismael Morejón Blasco <93212283+xSharkhy@users.noreply.github.com>
2024-03-20 17:38:27 +0100

    feat: create db
    minor:
     - updated routing on tsconfig
     - created layout file

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,45 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column, NOW } from 'astro:db'
 
-// https://astro.build/db/config
+const User = defineTable({
+  columns: {
+    _id: column.text(),
+    username: column.text({ primaryKey: true }),
+    password: column.text(),
+    email: column.text(),
+    role: column.text({ default: 'user' }),
+    birthday: column.date(),
+    createdAt: column.date({ default: NOW }),
+    updatedAt: column.date({ default: NOW })
+  }
+})
+
+const Post = defineTable({
+  columns: {
+    _id: column.text(),
+    title: column.text(),
+    content: column.text(),
+    author: column.text({ references: () => User.columns._id }),
+    hasJobPost: column.boolean({ default: false }),
+    createdAt: column.date({ default: NOW }),
+    updatedAt: column.date({ default: NOW })
+  }
+})
+
+const Contact = defineTable({
+  columns: {
+    _id: column.text(),
+    name: column.text(),
+    email: column.text(),
+    message: column.text(),
+    read: column.boolean({ default: false }),
+    createdAt: column.date({ default: NOW })
+  }
+})
+
 export default defineDb({
-  tables: {}
-});
+  tables: {
+    User,
+    Post,
+    Contact
+  }
+})
