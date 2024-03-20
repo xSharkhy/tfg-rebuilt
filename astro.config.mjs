a497592ae8925c070549ca6bd4017e2907694d47
Ismael Morejón Blasco <93212283+xSharkhy@users.noreply.github.com>
2024-03-20 23:05:33 +0100

    chore: formated standard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import db from "@astrojs/db";
-import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import db from '@astrojs/db'
+import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), db(), sitemap()],
-  output: "server",
+  output: 'server',
   adapter: vercel()
-});
+})
