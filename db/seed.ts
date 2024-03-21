b0742f86e24274f2ad18d77e63672298855d9326
Ismael Morejón Blasco <93212283+xSharkhy@users.noreply.github.com>
2024-03-21 12:46:06 +0100

    chore: start and seed db

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,21 @@
-import { db } from 'astro:db'
+import { db, User } from 'astro:db'
 
-// https://astro.build/db/seed
-export default async function seed () {
-  // TODO
+export default async function () {
+  await db.insert(User).values([
+    {
+      _id: crypto.randomUUID(),
+      username: 'admin',
+      password: 'admin',
+      email: 'admin@admin.com',
+      role: 'admin',
+      birthday: new Date('1990-01-01')
+    },
+    {
+      _id: crypto.randomUUID(),
+      username: 'user',
+      password: 'user',
+      email: 'user@user.com',
+      birthday: new Date('1990-01-01')
+    }
+  ])
 }
